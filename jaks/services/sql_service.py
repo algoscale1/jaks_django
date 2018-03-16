f6d19ff0fe6a6c4d27d32647c9461f888f0db8ff
Vartika <vartika.purohit@algoscale.com>
2018-03-16 15:54:21 +0530

    uncomment few line

--- jaks/services/sql_service.py
+++ jaks/services/sql_service.py
@@ -4,8 +4,8 @@ from jaks.models import UserProfile
 
 def save_user_data(user_name,email,password,dob,gender):
     try:
-        # user = User(username=user_name,email=email,password=password).save()
-        user = User.objects.get(id=1)
+        user = User(username=user_name,email=email,password=password).save()
+        # user = User.objects.get(id=1)
         print(user)
         print(dob,gender)
         UserProfile(user=user,date_of_birth=dob,gender=gender).save()
