ee77a21b4878a5881c31b2175e8118981f9ba05c
shubhendra <shubhendra.raj@algoscale.com>
2018-03-20 12:56:44 +0530

    files uploaded

--- jaks/service_urls.py
+++ jaks/service_urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from .view import index,Signup,Login
+from .view import index,Signup,Login,UserIndex
 # from django.conf import settings
 # from django.conf.urls.static import static
 from jaks.view.classify import TextClassifier
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^index',index.Index.as_view()),
     url(r'^signup',Signup.Signup.as_view()),
     url(r'^login', Login.Login.as_view()),
+    url(r'^userIndex', UserIndex.UserIndex.as_view()),
     url(r'^save_userdata',Signup.Signup.as_view()),
     url(r'^save_package',Signup.SavePackage.as_view()),
     url(r'^api_generator/$',Signup.ApiGenerator.as_view()),
